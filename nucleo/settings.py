ca520b03357d037407b7d80afa86a84eb3f84f7c
Mirko Olivares <mirkodaniel.olivares@gmail.com>
2024-11-10 02:39:43 -0300

    first commit

--- nucleo/settings.py
+++ nucleo/settings.py
@@ -77,6 +77,14 @@ WSGI_APPLICATION = 'nucleo.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'elriscocolorado',
         'USER': 'root',
